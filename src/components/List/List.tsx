431951b055d85e0720167773bf22a097497e6d94
Daria Nikulina <daryanickulina@yandex.ru>
2023-09-14 18:43:18 +0400

    feat: table styles

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,28 +1,34 @@
-import style from "./List.module.css";
+import styles from "./List.module.css";
 import ListRow from "../ListRow/ListRow";
 import { useContext } from "react";
-import { UserData, UsersContext } from "../../store/store";
+import { ThemeContext, UserData, UsersContext } from "../../store/store";
 
 export default function List() {
   const users = useContext(UsersContext);
+  const { theme } = useContext(ThemeContext);
 
   return (
-    <>
-      <table>
+    <div className={`${styles.tableBox} ${styles[theme]}`}>
+      <table className={styles.tableHeader}>
         <thead>
           <tr>
-            <th>Name</th>
-            <th>Age</th>
-            <th>Subscription</th>
-            <th>Employment</th>
+            <th className={styles.headerText}>Name</th>
+            <th className={styles.headerText}>Age</th>
+            <th className={styles.headerText}>Subscription</th>
+            <th className={styles.headerText}>Employment</th>
           </tr>
         </thead>
-        <tbody>
-          {users.users?.map((user: UserData) => (
-            <ListRow {...user} key={user.id} />
-          ))}
-        </tbody>
       </table>
-    </>
+
+      <div className={styles.scroll}>
+        <table className={styles.table}>
+          <tbody>
+            {users.users?.map((user: UserData) => (
+              <ListRow {...user} key={user.id} />
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 }
